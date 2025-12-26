e5ddef279078212966a11834978c1da6d1391e82
notunknownfr <136579713+notunknownfr@users.noreply.github.com>
2025-12-27 04:03:45 +0500

    Time: 19 ms (6.23%) | Memory: 19.3 MB (82.60%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -5,9 +5,10 @@ class Solution:
         for num in nums:
             if num in element_dict:
                 element_dict[num]+=1
+
+                
             else:
                 element_dict[num]=1
 
-        for element in element_dict:
-            if element_dict[element]>len(nums)/2:
-                return element
+            if element_dict[num]>len(nums)/2:
+                    return num
